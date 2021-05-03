eb97d5d998a591f34216e11bff3c0980b3f88bb9
Mayra Alejandra Rozo Barrera - Ceiba Software <alejandra.rozo@ceiba.com.co>
2021-05-03 13:43:59 -0500

    Agregando la creacion de la tabla poliza

--- microservicio/src/main/resources/db/migration/DDL/V1.0__schema.sql
+++ microservicio/src/main/resources/db/migration/DDL/V1.0__schema.sql
@@ -5,3 +5,18 @@ create table usuario (
  fecha_creacion datetime null,
  primary key (id)
 );
+
+create table poliza (
+    id int(11) not null auto_increment,
+    nombre_propietario varchar(100) not null,
+    telefono_propietario varchar(10) not null,
+    nombre_mascota varchar(50) not null,
+    tipo_mascota varchar(50) not null,
+    fecha_nacimiento_mascota date not null,
+    peso_mascota varchar(10) not null,
+    altura_mascota varchar(10) null,
+    valor_poliza double not null,
+    fecha_inicio datetime not null,
+    fecha_terminacion datetime not null,
+    primary key (id)
+);
