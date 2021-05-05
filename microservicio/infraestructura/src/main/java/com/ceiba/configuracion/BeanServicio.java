c0bbedf408a3655a7eb86dc34d40415b53346e28
Mayra Alejandra Rozo Barrera - Ceiba Software <alejandra.rozo@ceiba.com.co>
2021-05-04 22:04:02 -0500

    Eliminando las clases de usuario

--- microservicio/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
@@ -1,28 +1,12 @@
 package com.ceiba.configuracion;
 
 import com.ceiba.usuario.puerto.repositorio.RepositorioPoliza;
-import com.ceiba.usuario.puerto.repositorio.RepositorioUsuario;
 import com.ceiba.usuario.servicio.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanServicio {
-
-    @Bean
-    public ServicioCrearUsuario servicioCrearUsuario(RepositorioUsuario repositorioUsuario) {
-        return new ServicioCrearUsuario(repositorioUsuario);
-    }
-
-    @Bean
-    public ServicioEliminarUsuario servicioEliminarUsuario(RepositorioUsuario repositorioUsuario) {
-        return new ServicioEliminarUsuario(repositorioUsuario);
-    }
-
-    @Bean
-    public ServicioActualizarUsuario servicioActualizarUsuario(RepositorioUsuario repositorioUsuario) {
-        return new ServicioActualizarUsuario(repositorioUsuario);
-    }
 
     @Bean
     public ServicioCrearPoliza servicioCrearPoliza(RepositorioPoliza repositorioPoliza) {
--- microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
+++ microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
@@ -1,5 +1,3 @@
-insert into usuario(nombre,clave,fecha_creacion) values('test','1234',now());
-
 insert into poliza(
     nombre_propietario,
     telefono_propietario,
